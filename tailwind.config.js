b6166e3727ba2d75b329433f57b0aa248007ae65
PenguScripts <roccojohn306@gmail.com>
2024-10-28 19:35:06 -0400

    Update tailwind.config.js to include all file extensions in content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js, ts, tsx, jsx}"],
+  content: ["./src/**/*.{js,ts,tsx,jsx}"],
   theme: {
     extend: {},
   },
