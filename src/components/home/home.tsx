6f8071c8b0b6f2088d5172bae2eeed5ae9e910ef
PenguScripts <roccojohn306@gmail.com>
2024-10-30 23:11:26 -0400

    Refactor Home component layout and styles; enhance responsiveness and add Divider for better section separation

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -8,59 +8,56 @@ import {
 } from "react-icons/fi";
 import TestimonialSlider from "./TestimonialSlider";
 import React from "react";
-
+import Divider from "@mui/material/Divider"
 
 export const Home = React.memo(() => {
   return (
     <div className="container mx-auto px-4 py-8">
       {/* Hero Section */}
-      <section className="text-center lg:text-left mt-16">
-        <h1 className="text-4xl font-bold text-gray-100">Welcome to ZenFlow</h1>
-        <p className="mt-4 text-lg font-light text-gray-400">
+      <section className="text-center lg:text-left mt-16 px-4">
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-100">
+          Welcome to ZenFlow
+        </h1>
+        <p className="mt-4 text-base md:text-lg font-light text-gray-400">
           The most powerful tool for managing your projects and tasks.
         </p>
       </section>
+
       {/* Quick Links Section */}
-      <section className="mt-12">
-        <h2 className="text-2xl font-semibold mb-4 text-gray-200">
+      <section className="mt-12 px-4">
+        <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-200">
           Quick Links
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {/* Card 1: Tasks */}
-          <div className="bg-gray-800 hover:bg-gray-700 p-6 rounded-lg shadow-md text-gray-200 flex flex-row items-center space-x-4 transition-colors duration-200">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
+          {/* Quick Links Cards */}
+          <div className="bg-gray-800 hover:bg-gray-700 p-6 rounded-lg shadow-md text-gray-200 flex flex-col items-center md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4 transition-colors duration-200">
             <FiCheckSquare className="text-3xl text-blue-400" />
-            <div>
+            <div className="text-center md:text-left">
               <h3 className="text-lg font-semibold">Tasks</h3>
               <p className="text-sm text-gray-400">
                 Manage your tasks efficiently.
               </p>
             </div>
           </div>
-
-          {/* Card 2: Projects */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-md text-gray-200 flex flex-row items-center space-x-4 hover:bg-gray-700 transition-colors duration-200">
+          <div className="bg-gray-800 hover:bg-gray-700 p-6 rounded-lg shadow-md text-gray-200 flex flex-col items-center md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4 transition-colors duration-200">
             <FiFolder className="text-3xl text-green-400" />
-            <div>
+            <div className="text-center md:text-left">
               <h3 className="text-lg font-semibold">Projects</h3>
               <p className="text-sm text-gray-400">
                 Oversee all your project details.
               </p>
             </div>
           </div>
-
-          {/* Card 3: Goals */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-md text-gray-200 flex flex-row items-center space-x-4 hover:bg-gray-700 transition-colors duration-200">
+          <div className="bg-gray-800 hover:bg-gray-700 p-6 rounded-lg shadow-md text-gray-200 flex flex-col items-center md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4 transition-colors duration-200">
             <FiTarget className="text-3xl text-yellow-400" />
-            <div>
+            <div className="text-center md:text-left">
               <h3 className="text-lg font-semibold">Goals</h3>
               <p className="text-sm text-gray-400">Set and track your goals.</p>
             </div>
           </div>
-
-          {/* Card 4: Notes */}
-          <div className="bg-gray-800 p-6 rounded-lg shadow-md text-gray-200 flex flex-row items-center space-x-4 hover:bg-gray-700 transition-colors duration-200">
+          <div className="bg-gray-800 hover:bg-gray-700 p-6 rounded-lg shadow-md text-gray-200 flex flex-col items-center md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4 transition-colors duration-200">
             <FiEdit className="text-3xl text-red-400" />
-            <div>
+            <div className="text-center md:text-left">
               <h3 className="text-lg font-semibold">Notes</h3>
               <p className="text-sm text-gray-400">
                 Quickly jot down your ideas.
@@ -69,13 +66,14 @@ export const Home = React.memo(() => {
           </div>
         </div>
       </section>
+
       {/* Features Section */}
-      <div className="flex flex-col items-center">
-        <section className="mt-16 w-[50vw] flex-grow">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-200">
+      <div className="flex flex-col items-center px-4">
+        <section className="mt-16 w-full">
+          <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-200">
             Features
           </h2>
-          <div className="grid grid-rows-1 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-6">
             {/* Feature Cards */}
             <div className="flex items-center transition-all ease-in-out duration-200 hover:scale-105 bg-gray-800 hover:bg-gray-700 p-4 rounded-lg shadow-sm">
               <div className="flex-shrink-0 w-12 h-12 text-blue-400 rounded-full flex items-center justify-center text-2xl">
@@ -116,15 +114,30 @@ export const Home = React.memo(() => {
                 </p>
               </div>
             </div>
+            {/* Notes Feature Card */}
+            <div className="flex items-center transition-all ease-in-out duration-200 hover:scale-105 bg-gray-800 hover:bg-gray-700 p-4 rounded-lg shadow-sm">
+              <div className="flex-shrink-0 w-12 h-12 text-red-400 rounded-full flex items-center justify-center text-2xl">
+                <FiEdit />
+              </div>
+              <div className="ml-4">
+                <h3 className="text-lg font-semibold text-gray-100">
+                  Notes
+                </h3>
+                <p className="text-sm text-gray-400">
+                  Jot down ideas and information with ease.
+                </p>
+              </div>
+            </div>
           </div>
         </section>
 
-
-        <section className="mt-16 flex-grow">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-200">
+        <Divider sx={{bgcolor: "rgb(156, 163, 175)", my: "5vh"}} aria-hidden="true" variant="middle" flexItem />
+        {/* Testimonials Section */}
+        <section className="mt-8 w-full flex-col flex justify-center">
+          <h2 className="text-xl text-center md:text-2xl font-semibold mb-4 text-gray-200">
             Testimonials
           </h2>
-          <div className="w-[50vw]">
+          <div className="w-full md:w-[50vw] mx-auto">
             <TestimonialSlider />
           </div>
         </section>
